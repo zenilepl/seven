cc11bd983fb9cfe73d88ab735570222f454e0b04
zenilepl <45181384+zenilepl@users.noreply.github.com>
2018-12-13 19:34:09 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(Logged in as ${client.user.tag}!);
+});
+
+
+client.on('message', message => {
+    var prefix = "*";
+
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("f!bc <message>");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .addField('» السيرفر :', ${message.guild.name})
+                .addField('» المرسل : ', ${message.author.username}#${message.author.discriminator})
+                .addField(' » الرسالة : ', args)
+                .setColor('#ff0000')
+                // m.send([${m}]);
+                m.send(${m},{embed: bc});
+            });
+        }
+        } else {
+            return;
+        }
+    });
+	
+	client.login(process.env.BOT_TOKEN);
