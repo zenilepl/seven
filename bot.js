e3fa856344b94c18bb0e167189b89efd11d56cda
zenilepl <45181384+zenilepl@users.noreply.github.com>
2018-12-13 18:48:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,30 +8,30 @@ client.on('ready', () => {
 
 client.on('message', message => {
     var prefix = "*";
-
+    
         if (message.author.id === client.user.id) return;
         if (message.guild) {
        let embed = new Discord.RichEmbed()
         let args = message.content.split(' ').slice(1).join(' ');
     if(message.content.split(' ')[0] == prefix + 'bc') {
         if (!args[1]) {
-    message.channel.send("f!bc <message>");
+    message.channel.send("**f!bc <message>**");
     return;
     }
             message.guild.members.forEach(m => {
        if(!message.member.hasPermission('ADMINISTRATOR')) return;
                 var bc = new Discord.RichEmbed()
-                .addField('» السيرفر :', ${message.guild.name})
-                .addField('» المرسل : ', ${message.author.username}#${message.author.discriminator})
+                .addField('» السيرفر :', `${message.guild.name}`)
+                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
                 .addField(' » الرسالة : ', args)
                 .setColor('#ff0000')
-                // m.send([${m}]);
-                m.send(${m},{embed: bc});
+                // m.send(`[${m}]`);
+                m.send(`${m}`,{embed: bc});
             });
         }
         } else {
             return;
         }
     });
-	
+
 client.login(process.env.BOT_TOKEN);
