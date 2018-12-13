1a43990dc698493ef8b4c4b14004881487b29bf6
zenilepl <45181384+zenilepl@users.noreply.github.com>
2018-12-13 18:44:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 
@@ -34,4 +34,4 @@ client.on('message', message => {
         }
     });
 	
-	client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
